1a0fd89b14af02146c40aa00db1b44fb97e92944
Agnieszka Witek <witek.aga@gmail.com>
2021-07-01 21:18:49 +0200

    Add NavLinks

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import PageNav from '../PageNav';
 
 const MainLayout = (props) => (
   <div>
     {props.children}
+    <PageNav />
   </div>
 );
 
