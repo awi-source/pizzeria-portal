d239c9f3dff255f0051e3dbccca03a18c137f852
Agnieszka Witek <witek.aga@gmail.com>
2021-07-01 08:58:35 +0200

    Routing completed

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -2,7 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const MainLayout = (props) => (
-  <div>{props.children}</div>
+  <div>
+    {props.children}
+  </div>
 );
 
 MainLayout.propTypes = {
